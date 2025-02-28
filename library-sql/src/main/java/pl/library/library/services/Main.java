14c2eeb9aaba8cc7b7c9f8624813720aff3eb235
324sduked <sdudek324@gmail.com>
2025-02-28 12:04:15 +0100

    moved Main into package containing Services

--- library-sql/src/main/java/pl/library/library/services/Main.java
+++ library-sql/src/main/java/pl/library/library/services/Main.java
@@ -1,4 +1,4 @@
-package pl.library.library;
+package pl.library.library.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
